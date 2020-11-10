01295fc1f850e3515253c44dd808e403c1ae63be
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-10 19:56:20 +0530

    Update geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,18 +1,18 @@
-const request = require('request')
+const request=require('request')
 const geoCode = (address, callback) => {
-    const geCodeUrl='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic3VqaXRoOTYzMiIsImEiOiJja2hhNnNzNWoxMWo5MnFucTVmMGhmNXNtIn0.hlpzK2b7szZUhSI1_Bql6g&limit=1'
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic3VqaXRoOTYzMiIsImEiOiJja2hhNnNzNWoxMWo5MnFucTVmMGhmNXNtIn0.hlpzK2b7szZUhSI1_Bql6g&limit=1'
 
-    request({ url : geCodeUrl, json: true }, (error , response) =>{
+    request({ url, json: true }, (error , {body} = {}) =>{
         if (error) {
             callback('unable to connect to location server', undefined )
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('unable to find locaion ....!', undefined)
         }
         else{
             callback(undefined, {
-                longitude : response.body.features[0].center[0],
-                latitude : response.body.features[0].center[1],
-                location : response.body.features[0].place_name
+                longitude : body.features[0].center[0],
+                latitude : body.features[0].center[1],
+                location : body.features[0].place_name
             })
         }
     })
