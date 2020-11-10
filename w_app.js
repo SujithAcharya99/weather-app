3e88f8c95098b247aa799f164bfa32a65ea53f9b
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-10 20:00:59 +0530

    Update w_app.js

--- w_app.js
+++ w_app.js
@@ -1,8 +1,25 @@
-const request = require('request')
 const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
+const address = process.argv[2]
 
-geocode('suratkal india',(error, data) => {
-    console.log('error',error)
-    console.log('data:',data)
+if (!address) {
+    console.log('please provide an address....!')
+} else{
+
+    geocode(address,(error, {latitude , longitude , location} = {}) => {
+    if (error) {
+        return console.log(error)
+        
+    }
+     forecast(latitude, longitude, (error, fdata) => {    
+        if (error) {
+            return console.log('Error :', error)
+            
+        }
+         
+       console.log('Location Data :', location)
+        console.log('Weather Data :', fdata)
+      }) 
 })
+}
